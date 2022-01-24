fe44386b091248355e0606d92ea4da949d891e45
Jessica Dubem <jessicadubem@jesswebcreator.com>
2022-01-24 09:35:42 +0100

    created reusable component files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Contact from './components/reusable/contact';
+import Footer from './components/reusable/footer';
+import Navigation from './components/reusable/navigation';
 
 function App() {
   return (
